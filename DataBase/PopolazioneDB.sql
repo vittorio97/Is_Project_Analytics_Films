66433b352efe5cda6e4faab7013af2e109288199
vittorio97 <44083374+vittorio97@users.noreply.github.com>
2019-02-14 12:01:38 +0100

    DataBase

---
+++ DataBase/PopolazioneDB.sql
@@ -0,0 +1,69 @@
+use progettois;
+
+insert into Utente  values
+     ("Vincenzo.armenio1@gmail.com","Vincenzo1","Unisa","Amministratore"),
+     ("Alina.aprea2@hotmail.it","Alina2","Unisa2","Utente registrato"),
+     ("Alberto.girotondo@libero.it","AlbertoG2","AlbyGiro","Utente registrato"),
+     ("Lucia.pesca@gmail.com","LuciaPesca1","LuciiP","Visitatore"),
+     ("Luchetto98@gmail.com","Luchetto98","Unisa1","Visitatore");
+     
+
+insert into FILM  values
+    ("Tremors","Fanno la loro terribile apparizione alcuni voraci serpentoni sotterranei. Ciechi, ma sensibili al più piccolo rumore, i vermi colpiscono senza scampo.","../locandine/Tremors.jpg","Horror","Vincenzo.armenio1@gmail.com"),
+    ("ToyStory","La vita di un cowboy giocattolo tanto amato da un bambino è minacciata dall'arrivo di un nuovo robot colorato e pieno di luci.","../locandine/ToyStory.jpg","Fantastico","Vincenzo.armenio1@gmail.com"),
+	("Shutter Island","Rachel Salado, paziente del manicomio criminale sull'isola di Shutter, scompare all'improvviso. L'agente federale Teddy Daniels, affiancato dall'ufficiale Chuck Aule, deve indagare per risolvere il mistero.","../locandine/ShutterIsland.jpg","Thriller","Vincenzo.armenio1@gmail.com"),
+	("Gifted","Uno zio single intraprende una battaglia legale per ottenere la custodia della nipote, giovane prodigio della matematica, e garantirle una vita normale nonostante l'opposizione della madre.","../locandine/Gifted.jpg","Drammatico","Vincenzo.armenio1@gmail.com"),
+	("Avengers", "I leggendari supereroi Iron Man, Hulk, Thor, Capitan America, Occhio di Falco e la Vedova Nera vengono reclutati da un'agenzia governativa segreta per combattere un nemico inatteso che minaccia la sicurezza della Terra","../locandine/Avengers.jpg","Fantastico","Vincenzo.armenio1@gmail.com"),
+    ("Illusionist","In un mondo dove nulla è come appare, un illusionista di nome Einsenheim e l'ispettore di polizia Uhl, si confrontano in una sfida di volontà nel cercare di determinare dove finisce la realtà e dove inizia la magia.","../locandine/Illusionist.jpg","Drammatico","Vincenzo.armenio1@gmail.com"),
+	("Resta con me", "Un violento uragano distrugge il veliero sul quale viaggiano Tami e Richard. A causa delle gravi ferite riportate, la donna è costretta a prendere il comando dell'imbarcazione e salvare entrambi.", "../locandine/RestaconMe.jpg","Drammatico","Vincenzo.armenio1@gmail.com"),
+    ("The imitation game", "La vita del matematico inglese Alan Turing, genio indiscusso del XX secolo, considerato uno dei padri dell'informatica e dei moderni computer, fino alla sua precoce e tragica scomparsa.","../locandine/TheImitationGame.jpg", "Drammatico","Vincenzo.armenio1@gmail.com"),
+	("Glass","L'orda, ossia Kevin Wendell Crumb e le sue altre numerose personalità,ha catturato un nuovo gruppo di ragazze e si prepara a sacrificarle alla Bestia. È però sulle sue tracce il vigilante David Dunn, che grazie all'aiuto del figlio e alle sue visioni psichiche arriva presto a un confronto con il feroce avversario. Entrambi però finiscono catturati rinchiusi in un istituto psichiatrico, lo stesso dove da 19 anni è prigioniero l'uomo di vetro, il geniale Elijah Price..","../locandine/glass.jpg","Thriller","Vincenzo.armenio1@gmail.com"),
+	("Suspiria","Un'oscurità turbina al centro di una compagnia di danza di fama mondiale, che inghiottirà il direttore artistico della troupe,un'ambiziosa giovane ballerina e una dolorosa psicoterapeuta. Alcuni soccomberanno all'incubo, altri finalmente si sveglieranno.","../locandine/suspiria.jpg", "Horror", "Vincenzo.armenio1@gmail.com"),
+    ("Bohemian Rapsody","Dopo anni di annunci e successive proroghe, protagonisti mancati e sceneggiature bocciate, prende corpo finalmente il biopic su Freddie Mercury e i Queen. Il film di Bryan Singer concentra la propria attenzione su un arco temporale lungo quindici anni, partendo dagli inizi della band nei primi anni Settanta fino alla memorabile esibizione del 13 Luglio 1985 a Wembley per il Live Aid.","../locandine/bohemianrhapsody.jpg","Documentario","Vincenzo.armenio1@gmail.com"),
+    ("Roma","Roma, il film diretto da Alfonso Cuaron, racconta un anno turbolento della vita di una famiglia borghese nella Città del Messico degli anni 70,Roma è un ritratto di vita vera, intimo e toccante, raccontato attraverso le vicende di una famiglia che cerca di preservare il proprio equilibrio in un momento di lotta personale, sociale e politica.","../locandine/roma.jpg","Drammatico","Vincenzo.armenio1@gmail.com"),
+    ("A star is born","In questa nuova rivisitazione della tormentata storia d'amore, Cooper interpreta Jackson Maine, musicista di successo che si sta però avviando sul viale del tramonto che scopre, e si innamora della squattrinata artista Ally. Lei ha da poco chiuso in un cassetto il suo sogno di diventare una grande cantante, fino a quando Jack la convince a tornare sul palcoscenico. Ma mentre la carriera di Ally inizia a spiccare il volo, il lato privato della loro relazione sta perdendo colpi a causa della battaglia che Jack conduce contro i suoi demoni interiori.","../locandine/star.jpg","Musicale","Vincenzo.armenio1@gmail.com"),
+    ("Aquaman","Arthur Curry viene a sapere che è l'erede del regno sottomarino di Atlantide e deve farsi avanti per guidare il suo popolo ed essere un eroe per il mondo.","../locandine/aquaman.jpg","Fantastico","Vincenzo.armenio1@gmail.com");
+    
+    
+    
+insert into RECENSIONE values
+("Gradevole","Tremors è un film trash in contrasto con un epoca di trash al contrario, stabilito ciò lo si può valutare lucidamente. Tremors non è un film spocchioso che nasconde chissà quali allegorie sull'esistenza e non è un gioiello visivo fatto di pura plastica. Tremors(almeno questo capitolo) è un gradevole filmetto che paga un tributo agli anni 50 , ciò alla fine di un decennio più o meno horror legato ai teenagers. Qui cambia tutto, qui i protagonisti sono degli sbandati, solitari reietti in un deserto, spostati e derelitti, questo è il punto interessante al di là della semplice visione, un attacco(inconsapevolmente?) iconoclasta alle dicotomie di un epoca in un mix che ricorda lo scontro tra un John Wayne alcolizzato e disgraziato con il mostro della palude, e qui è il punto, riscoprire a piene mani il passato per staccarsi dai dogmi, questo è il pregio del film.
+","Alina.aprea2@hotmail.it","Tremors"),
+
+("Drammatico e dolcissimo","Un uomo ossessionato dalla autorevolezza dittatoriale della madre,decide di vivere una vita semplice,ad un certo punto della vita riceve un dono,la figlia della sorella morta, è l'ultimo desiderio della sorella, lasciarla al fratello significa per lei farla vivere e crescere come una bambina normale,ma la piccola Mary all'età di sei sette anni si rivela una gifted, un genio in matematica così come la madre,essa non può vivere la scuola come gli altri,ma deve vivere la sua infanzia fatta di spensieratezza,e soprattuto confrontarsi con altri bambini.Ritengo questo film una grandissima testimonianza di un problema poco conosciuto,realmente vissuto nel mondo da migliaia di famiglie,la convivenza con un figlio super dotato,anche se la definizione può sembrare odiosa è l'esatta definizione che si può dare,riuscire nel trovare la giusta dimensione non è facile in quanto sia le istituzioni e i media non conoscendo il problema creano un muro di emarginazione sociale verso questi fantastici bambini,che cercano soltanto di vivere la loro vita
+","Alina.aprea2@hotmail.it","Gifted"),
+("Deludente","Il film avrebbe dovuto essere incentrato sulla bambina particolarmente dotata, ma l'illusione dura poco. Non vengono quasi per nulla trattate le tematiche legate ai gifted children, al centro rimangono sempre i grandi e i loro grandi problemi. Alla distanza a uscire sono i soliti clichè hollywoodiani, fatti di improbabili relazioni sentimentali tra professoresse e genitori degli alunni, famiglie che si combattono nei tribunali a colpi di arringhe e odi atavici, e tate di colore pacioccose e piene di umanità.Musiche eccessive e attori poco convincenti completano un film che non ha saputo dare ciò che prometteva.
+","Alberto.girotondo@libero.it","Gifted"),
+
+("Thriller psicologico di alta scuola"," Thriller di altissima scuola sfornato da un grandissimo Scorsese che si conferma uno dei migliori registi viventi. Pellicola mai banale e scontata con la quale il maestro italo-americano cerca di colpire lo spettatore con giochi psicologici e ripetuti colpi di scena, tanto da porre un punto interrogativo sul film stesso, il quale si appresta a diverse interpretazioni. Uno dei migliori film del genere; la sua visione e comprensione a 360 gradi necessitano di un attenzione ai particolari maniacale...geniale la regia che per più di un ora prende in giro lo spettatore che vede il tutto con gli occhi della pazzia, tanto da convincerlo a pensare come il pazzo al punto tale che tutto il film viene messo in discussione.
+","Alberto.girotondo@libero.it","Shutter Island"),
+
+("Spettacolare!","Ho atteso con impazienza la visione di questo film perché ero certo che avrei provato delle emozioni fortissime. Non mi ha delus, anzi, mi ha lasciato a bocca aperta. Sono isolano e sono cresciuto andando in barca, ma un'esperienza come quella vissuta dalla protagonista ha che dell'incredibile. Musiche, effetti speciali ed attori: tutto perfetto. Correre al cinema!
+","Alberto.girotondo@libero.it","Resta con me");
+
+
+  
+  
+insert into CINEMA values
+
+    ("Movieplex","Mercogliano","18:00:00"),
+    ("Duel","Salerno","19:00:00"),
+	("Savoia","Napoli","20:00:00");
+   
+    
+    insert into CINEMA_FILM values
+	("Movieplex","Tremors"),
+	("Movieplex","ToyStory"),
+	("Movieplex","Shutter Island"),
+    ("Movieplex","Illusionist"),
+    ("Movieplex","Resta con me"),
+    ("Duel","Tremors"),
+    ("Duel","ToyStory"),
+    ("Duel","Gifted"),
+    ("Duel","Avengers"),
+    ("Savoia","Tremors"),
+    ("Savoia","ToyStory"),
+    ("Savoia","Shutter Island"),
+    ("Savoia","Resta con me"),
+    ("Savoia","Gifted"),
+    ("Savoia","The imitation game");
---
+++ DataBase/progettoIS.sql
@@ -0,0 +1,60 @@
+create schema progettoIS;
+use progettoIS;
+
+CREATE TABLE UTENTE (
+	Email varchar(30) primary key,
+    Username varchar(30),
+    Pass varchar(20),
+    Ruolo varchar(20)
+    );
+    
+         
+CREATE TABLE FILM (
+	TitoloFilm varchar(30) primary key,
+    Trama varchar(1500),
+    Locandina varchar(40),
+    Categoria varchar(20),
+    Email varchar(30),
+    
+    foreign key(Email) references UTENTE(Email)
+    on delete set null on update cascade
+    
+    );
+      
+    
+    
+CREATE TABLE RECENSIONE (
+	Titolo varchar(50) primary key,
+    Testo varchar(1500),
+    email varchar(30),
+    titoloFilm varchar(30),
+	
+    foreign key(email) references Utente(Email)
+      on delete cascade on update cascade,
+    foreign key(TitoloFilm) references Film(TitoloFilm)
+      on delete cascade on update cascade
+    );
+	    
+
+
+CREATE TABLE CINEMA (
+	Nome varchar(20) primary key,
+    Luogo varchar(20),
+    Orario time
+
+    );
+    
+    
+    
+  
+ CREATE TABLE CINEMA_FILM  (
+	Nome varchar(20) ,
+    TitoloFilm varchar (30),
+    primary key(Nome,TitoloFilm),
+    foreign key(Nome) references CINEMA(Nome),
+    foreign key(TitoloFilm) references FILM(TitoloFilm)
+    on delete cascade on update cascade
+    );
+    
+    
+    
